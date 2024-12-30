b729e2fc3ffcc0a1b251de99fa45285d752f42ff
sd99park <sd99park@gmail.com>
2024-12-29 23:07:33 -0600

    Add UpdatedAt to KTC table

--- data/DataQueries.py
+++ data/DataQueries.py
@@ -1,4 +1,5 @@
 from.DataAccess import DataAccess
+from datetime import datetime, timezone
 
 class DataQueries(DataAccess):
 
@@ -8,8 +9,8 @@ class DataQueries(DataAccess):
 
     def insert_ktc_data(self, id, first_name, last_name, position, ranking_one_qb, ranking_two_qb, json):
         query = f'''
-            INSERT INTO "KeepTradeCutRanking" ("Id", "FirstName", "LastName", "Position", "RankingOneQB", "RankingTwoQB", "Resource")
-            VALUES ('{id}', '{first_name}', '{last_name}', '{position}', '{ranking_one_qb}', '{ranking_two_qb}', '{json}');
+            INSERT INTO "KeepTradeCutRanking" ("Id", "FirstName", "LastName", "Position", "RankingOneQB", "RankingTwoQB", "Resource", "UpdatedAt")
+            VALUES ('{id}', '{first_name}', '{last_name}', '{position}', '{ranking_one_qb}', '{ranking_two_qb}', '{json}', '{datetime.now(timezone.utc)}');
         '''
         self.run_sql(query)
 
